1d6d5704febc5c8109b19d8e947227e8fd8e1b38
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-02 11:39:01 +0000

    2 Apr 2023, 17:09

--- src/PhoneCharger.ts
+++ src/PhoneCharger.ts
@@ -1,8 +1,9 @@
 import IPort, { IAppliance } from './interfaces/IPort';
 
 class PhoneCharger implements IPort{
-  
+
   providePower(appliance: IAppliance) {
+    
     throw new Error('Method not implemented.');
   }
   
